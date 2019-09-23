3b85db80a8f5923bb7c267bec8451cefc74a7632
Scott Reynen <scott@slicedbreadlabs.com>
2019-09-23 08:11:36 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pywell',
+    version='0.1',
+    author='Scott Reynen',
+    packages=find_packages(),
+    url='https://github.com/sreynen/pywell',
+    license='MIT',
+    description="A collection of independent Python scripts following the Unix philosphy of doing one thing well.",
+)
