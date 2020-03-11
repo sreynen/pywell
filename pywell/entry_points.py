ebfbc4dbbb21d43e7af876af87b8ca3bdaf2c9be
Scott Reynen <scott.reynen@moveon.org>
2020-03-11 06:54:10 -0600

    add run_from_api_gateway

--- pywell/entry_points.py
+++ pywell/entry_points.py
@@ -51,7 +51,7 @@ def json_serial(obj):
 
 def run_from_lamba(func, description, definitions, required, event) -> str:
     """
-    Entry point from Amazom Lambda.
+    Entry point from Amazon Lambda.
     """
     import json
     import os
@@ -72,4 +72,77 @@ def run_from_lamba(func, description, definitions, required, event) -> str:
 
     args = Struct(**event)
 
-    return json.dumps(func(args), default=json_serial)
+    if all_required_args_set(args, required, definitions):
+        return json.dumps(func(args), default=json_serial)
+
+
+def run_from_api_gateway(func, description, definitions, required, event, format='JSON', filename='') -> str:
+    """
+    Entry point from Amazon Lambda via API Gateway.
+    """
+    import json
+    import os
+    from urllib.parse import parse_qsl
+
+    if os.path.exists(os.path.join(os.getcwd(), 'settings.py')):
+        import settings
+    else:
+        settings = {}
+
+    kwargs = event.get('kwargs', False)
+    if kwargs:
+        for argname in kwargs.keys():
+            event[argname] = kwargs.get(argname)
+
+    if event.get('httpMethod', '') == 'POST':
+        try:
+            post_body = json.loads(event.get('body', '{}'))
+        except:
+            try:
+                post_body = dict(parse_qsl(event.get('body', '')))
+            except:
+                post_body = {}
+        for argname, helptext in definitions.items():
+            if not event.get(argname, False) and argname in post_body.keys():
+                event[argname] = post_body.get(argname, False)
+
+    for argname, helptext in definitions.items():
+        if not event.get(argname, False):
+            event[argname] = getattr(settings, argname, False)
+
+    args = Struct(**event)
+
+    if all_required_args_set(args, required, definitions):
+        result = func(args)
+        if format == 'CSV':
+            import csv
+            import io
+            if len(result):
+                headers = result[0].keys()
+            else:
+                headers = []
+            output = io.StringIO()
+            writer = csv.DictWriter(
+                output,
+                quoting=csv.QUOTE_NONNUMERIC,
+                fieldnames=headers
+            )
+            writer.writeheader()
+            for data in result:
+                writer.writerow(data)
+            return {
+                'isBase64Encoded': True,
+                'statusCode': 200,
+                'headers': {
+                    'Content-Type': 'text/csv',
+                    'Content-Disposition': 'attachment;filename=%s' % filename
+                },
+                "body": output.getvalue()
+            }
+        else:
+            return {
+                'isBase64Encoded': False,
+                'statusCode': 200,
+                'headers': {'Content-Type': 'application/json'},
+                "body": json.dumps(result)
+            }
